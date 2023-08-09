e8672ecb7e6b492690ef93641c5b6bf91979680d
thuanphat1501 <thuanphat1501@gmail.com>
2023-08-09 21:53:21 +0700

    first commit

---
+++ ros2_bot/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'ros2_bot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='server',
+    maintainer_email='server@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
+
